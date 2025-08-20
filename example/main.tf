1480d5f52fbe459a335f3d991d0d9b53cc14cdd5
jnikita19 <Nikita.Joshi.snaatak@mygurukulam.co>
2025-08-20 14:39:49 +0530

    updated files

---
+++ example/main.tf
@@ -0,0 +1,69 @@
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+# module "standard_tags" {
+#   source  = "git@github.com:OT-CLOUD-KIT/terraform-aws-standard-tagging.git?ref=dev"
+#   env     = var.env
+#   app     = var.app
+#   bu      = var.bu
+#   program = "edcil"
+#   team    = "lspl"
+#   region  = "us-east-1"
+# }
+
+module "assume_role" {
+  source = "../"
+
+  env = var.env
+  app = var.app
+  owner = var.owner
+  # bu  = var.bu
+
+  # policies_tags =var.tags
+  # roles_tags    = var.tags
+  
+  create_iam_roles       = var.create_iam_roles
+  create_iam_policies    = var.create_iam_policies
+  attach_policy_arns     = var.attach_policy_arns
+  attach_inline_policies = var.attach_inline_policies
+
+roles = [
+  for role in var.roles : {
+    name = role.name
+    path = role.path
+    desc = role.desc
+
+    trust_policy = {
+      policy_template_file = "${path.root}/${role.trust_policy.policy_template_file}"
+      policy_template_vars = merge(
+        try(role.trust_policy.policy_template_vars, {}),
+        {
+          account_id         = data.aws_caller_identity.current.account_id,
+          region             = data.aws_region.current.id
+        }
+      )
+    }
+
+    policies    = role.policies
+    policy_arns = role.policy_arns
+  }
+]
+
+
+ 
+  policies = [
+    for policy in var.policies : {
+      name                 = policy.name
+      path                 = policy.path
+      desc                 = policy.desc
+      policy_template_file = policy.policy_template_file
+      policy_template_vars = merge(
+        try(policy.policy_template_vars, {}),
+        {
+          account_id = data.aws_caller_identity.current.account_id
+          region     = data.aws_region.current.id
+        }
+      )
+    }
+  ]
+}
---
+++ example/terraform.tfvars
@@ -0,0 +1,58 @@
+env    = "Dev"
+app    = "Otcloudkit"
+owner = "opstree"
+max_session_duration   = 3600
+create_iam_roles       = true
+create_iam_policies    = true
+attach_policy_arns     = true
+attach_inline_policies = true
+
+policies = [
+  {
+    name                 = "ec2-access"
+    path                 = null
+    desc                 = "Allows full access to all EC2 resources and actions."
+    policy_template_file = "policy-documents/ec2-full-access-policy.tpl"
+  },
+
+  {
+    name                 = "s3-access"
+    path                 = null
+    desc                 = "Allows full access to all S3 buckets and related actions."
+    policy_template_file = "policy-documents/s3-policy.tpl"
+  }
+]
+
+roles = [
+  {
+    name = "ec2-access"
+    path = "/"
+    desc = "IAM role that can be assumed by IAM user 'Mohit' to manage EC2 and S3 resources."
+    trust_policy = {
+      policy_template_file = "policy-documents/assume-role-trust.tpl"
+      policy_template_vars = {
+        assume_type = "user"           # or "role"
+        assume_name = "Nikita"
+        account_id  = "266408865630"
+      }
+    }
+    policies    = ["ec2-access", "s3-access"]
+    policy_arns = [ "arn:aws:iam::aws:policy/ReadOnlyAccess" ]
+  },
+
+  # {
+  #   name = "s3-access"
+  #   path = "/"
+  #   desc = "IAM role that can be assumed by role 'demo-a' to access S3 resources with limited (read-only) permissions."
+  #   trust_policy = {
+  #     policy_template_file = "policy-documents/assume-role-trust.tpl"
+  #     policy_template_vars = {
+  #       assume_type = "role"
+  #       assume_name = "demo-a"
+  #       account_id  = "509633460021"
+  #     }
+  #   }
+  #   policies    = ["s3-access"]
+  #   policy_arns = [ "arn:aws:iam::aws:policy/ReadOnlyAccess" ]
+  # }
+]
